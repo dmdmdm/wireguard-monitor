62c53820ef3afcb66df70c18b3b6f3583b1f7c7c
dmdmdm <3187057+dmdmdm@users.noreply.github.com>
2025-06-08 12:32:43 -0400

    More colors

--- src/my_style.rs
+++ src/my_style.rs
@@ -8,19 +8,48 @@ use cursive::style::Effects;
 use cursive::theme::Effect;
 use cursive::utils::span::SpannedString;
 
-pub fn emphasis_style() -> Style {
-    let color_style = ColorStyle::new(Yellow.dark(), ColorType::InheritParent);
+pub fn yellow_style() -> ColorStyle {
+    return ColorStyle::new(Yellow.dark(), ColorType::InheritParent);
+}
+
+pub fn bold_yellow_style() -> Style {
     let effects = Effects::only(Effect::Bold);
     let style = Style {
         effects: effects,
-        color: color_style
+        color: yellow_style()
+    };
+    return style;
+}
+
+pub fn green_style() -> ColorStyle {
+    return ColorStyle::new(Green.dark(), ColorType::InheritParent);
+}
+
+pub fn bold_green_style() -> Style {
+    let effects = Effects::only(Effect::Bold);
+    let style = Style {
+        effects: effects,
+        color: green_style()
     };
     return style;
 }
 
 #[allow(dead_code)]
-pub fn emphasis_string(text: &str) -> SpannedString<Style> {
-    return SpannedString::styled(text, emphasis_style());
+pub fn yellow_string(text: &str) -> SpannedString<Style> {
+    return SpannedString::styled(text, yellow_style());
+}
+
+pub fn bold_yellow_string(text: &str) -> SpannedString<Style> {
+    return SpannedString::styled(text, bold_yellow_style());
+}
+
+pub fn bold_green_string(text: &str) -> SpannedString<Style> {
+    return SpannedString::styled(text, bold_green_style());
+}
+
+#[allow(dead_code)]
+pub fn green_string(text: &str) -> SpannedString<Style> {
+    return SpannedString::styled(text, green_style());
 }
 
 pub fn plain_string(text: &str) -> SpannedString<Style> {
